d2032e1761a0e001bb0b6a58c6d660c86532b8ac
Adrián Bolonio <adrian.bolonio@willhaben.at>
2021-02-22 10:21:30 +0100

    added a11y HTML errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Image } from "./components/Image";
+import React from "react"
+import ReactDOM from "react-dom"
+import { Image } from "./components/Image"
 
 if (process.env.NODE_ENV !== "production") {
-  const axe = require("@axe-core/react");
-  axe(React, ReactDOM, 1000);
+  const axe = require("@axe-core/react")
+  axe(React, ReactDOM, 1000)
 }
 
 function App() {
   return (
     <div>
       <Image />
+      <div role="myRole">This div has a wrong rule</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
