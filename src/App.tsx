7eeee2dc7553b3e5bc0595fdc5c0a4d5631d63f3
Adrián Bolonio <bolonio@github.com>
2022-04-20 15:53:50 +0200

    Remove element with wrong role

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { Image } from "./components/Image"
 
 function App() {
-  return (
-    <>
-      <Image />
-      <div role="myRole">This div has a wrong rule</div>
-    </>
-  )
+  return <Image />
 }
 
 export default App
