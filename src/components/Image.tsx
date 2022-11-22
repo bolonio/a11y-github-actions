8d4c3424b0db1d00eec0ac427e815a15ac21c8d2
Dhruvit Galoriya <dhruvitgaloriya@gmail.com>
2022-11-22 10:42:09 +0530

    push

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,5 +1,7 @@
-import image from './image.jpg'
+import image from "./image.jpg"
 
-export const Image = ({alt, role}: {alt?: string; role?: string}) => (
-  <img src={image} role={role} alt={alt} width='600px' />
+export const Image = ({ alt, role }: { alt?: string; role?: string }) => (
+  <div className="a11y" tabIndex="-1">
+    <img src={image} role={role} alt={alt} width="600px" />
+  </div>
 )
